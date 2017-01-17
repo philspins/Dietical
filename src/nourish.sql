57c8346c2329afd8526a90e8951932b893319549
Phil <philly.craig@gmail.com>
2017-01-17 11:10:00 -0500

    Update nourish.sql

--- src/nourish.sql
+++ src/nourish.sql
@@ -1,26 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jan 17, 2017 at 10:59 AM
--- Server version: 5.7.16-0ubuntu0.16.04.1
--- PHP Version: 7.0.13-0ubuntu0.16.04.1
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `nourish`
 --
 
 CREATE DATABASE nourish;
+USE nourish;
 
 -- --------------------------------------------------------
 
